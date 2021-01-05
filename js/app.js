e2d00fa214da00a360558273eec19d0c022f39f8
Juan antonio <jacc_1297@hotmail.com>
2021-01-05 11:29:19 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,157 @@
+const container = document.querySelector(".container");
+const resultado = document.querySelector("#resultado");
+const formulario= document.querySelector("#formulario");
+
+window.addEventListener("load",()=>{
+
+    formulario.addEventListener("submit",buscarClima);
+
+});
+
+
+function buscarClima(e){
+    e.preventDefault();
+
+    //validar
+    const ciudad = document.querySelector("#ciudad").value;
+    const pais = document.querySelector("#pais").value;
+
+    if(ciudad === "" || pais === ""){
+        //Hubo un error
+        mostrarError("Ambos campos son obligatorios!");
+        return;
+    }
+
+    //consultar API
+    consultarAPI(ciudad,pais);
+}
+
+function mostrarError(mensaje){
+
+  const alerta = document.querySelector(".bg-red-100");
+
+
+  if(!alerta){
+
+        //crear una alerta
+
+        const alerta = document.createElement("div");
+        alerta.classList.add("bg-red-100","border-red-400","text-red-700","px-4","py-3","rounded","max-w-md","mx-auto","mt-6","text-center");
+    
+        alerta.innerHTML = `
+        
+        <strong class="">Error!</strong>
+        <span class="block font-bold">${mensaje}</span>
+        
+        `;
+    
+        container.appendChild(alerta);
+
+        //se elimina la alertaa despues de 5seg
+
+        setTimeout(() => {
+            alerta.remove();
+        }, 4000);
+
+  }
+    
+
+
+}
+
+function consultarAPI(ciudad,pais){
+
+    const appID = "e10c0590df2e9a32a3652641b9358125";
+    
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appID}`;
+
+    spinner(); //muestra un spinner de carga
+
+    fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+
+        limpiarHTML(); //limpiar html previo
+
+        if(datos.cod === "404"){
+            mostrarError("Ciudad no encontrada");
+            return;
+        }
+
+        //Imprimir respuesta en el HTML
+        mostrarClima(datos);
+    })
+
+}
+
+function mostrarClima(datos){
+
+    const {name,main:{temp,temp_max,temp_min}} = datos;
+
+    const centigrados = kelvinACentigrados(temp);
+    const max = kelvinACentigrados(temp_max);
+    const min = kelvinACentigrados(temp_min);
+
+    const nombreCiudad = document.createElement("p");
+    nombreCiudad.textContent = `${name}`;
+    nombreCiudad.classList.add("font-bold","text-4xl");
+
+    const actual = document.createElement("p");
+    actual.innerHTML = `${centigrados} &#8451;`;
+    actual.classList.add("font-bold","text-6xl");
+
+    const tempMaxima = document.createElement("p");
+    tempMaxima.innerHTML= `Max: ${max} &#8451;`;
+    tempMaxima.classList.add("text-xl");
+
+    const tempMinima = document.createElement("p");
+    tempMinima.innerHTML= `Min: ${min} &#8451;`;
+    tempMinima.classList.add("text-xl");
+
+    const resultadoDiv = document.createElement("div");
+    resultadoDiv.classList.add("text-center","text-white");
+    resultadoDiv.appendChild(nombreCiudad);
+    resultadoDiv.appendChild(actual);
+    resultadoDiv.appendChild(tempMaxima);
+    resultadoDiv.appendChild(tempMinima);
+    resultado.appendChild(resultadoDiv);
+
+
+}
+
+const kelvinACentigrados = (grados) => parseInt(grados- 273.15);
+
+
+
+function limpiarHTML(){
+    while (resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function spinner() {
+
+    limpiarHTML();
+
+    const divSpinner = document.createElement("div");
+    divSpinner.classList.add("sk-fading-circle");
+
+    divSpinner.innerHTML = `
+    
+  <div class="sk-circle1 sk-circle"></div>
+  <div class="sk-circle2 sk-circle"></div>
+  <div class="sk-circle3 sk-circle"></div>
+  <div class="sk-circle4 sk-circle"></div>
+  <div class="sk-circle5 sk-circle"></div>
+  <div class="sk-circle6 sk-circle"></div>
+  <div class="sk-circle7 sk-circle"></div>
+  <div class="sk-circle8 sk-circle"></div>
+  <div class="sk-circle9 sk-circle"></div>
+  <div class="sk-circle10 sk-circle"></div>
+  <div class="sk-circle11 sk-circle"></div>
+  <div class="sk-circle12 sk-circle"></div>
+    
+    `;
+
+    resultado.appendChild(divSpinner);
+}
